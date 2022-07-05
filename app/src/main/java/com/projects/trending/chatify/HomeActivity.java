ea5991e8e5872c32842ac722204d9582ec8a24d8
SAMYAK99 <singhsamyak199@gmail.com>
2022-07-06 00:36:54 +0530

    Initial Project Setup and Developed Splash Screen

---
+++ app/src/main/java/com/projects/trending/chatify/HomeActivity.java
@@ -0,0 +1,23 @@
+package com.projects.trending.chatify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        if(firebaseAuth.getCurrentUser() == null){
+            startActivity(new Intent(HomeActivity.this ,RegistrationActivity.class));
+        }
+    }
+}
---
+++ app/src/main/java/com/projects/trending/chatify/MainActivity.java
@@ -0,0 +1,29 @@
+package com.projects.trending.chatify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, 3000);
+    }
+}
