b051538cc8dde8e2eb93594568b117b8276df514
SAMYAK99 <singhsamyak199@gmail.com>
2022-07-06 01:39:48 +0530

    UI Design for login and SignUp

--- app/src/main/java/com/projects/trending/chatify/LoginActivity.java
+++ app/src/main/java/com/projects/trending/chatify/LoginActivity.java
@@ -2,13 +2,25 @@ package com.projects.trending.chatify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView tv_signUp ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tv_signUp = findViewById(R.id.tv_signup);
+        tv_signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegistrationActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/projects/trending/chatify/RegistrationActivity.java
+++ app/src/main/java/com/projects/trending/chatify/RegistrationActivity.java
@@ -2,13 +2,26 @@ package com.projects.trending.chatify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    TextView tv_signIn ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        tv_signIn = findViewById(R.id.tv_signup);
+        tv_signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+            }
+        });
     }
 }
